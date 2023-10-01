65fe825e7a8715c255c17fab44769fcdb75ff69b
Sevinda Perera <sevindacharukaperera@gmail.com>
2023-10-01 20:57:57 +0530

    normal material

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -10,7 +10,8 @@ const Box = () => {
   return (
     <mesh rotation={[90, 0, 90]}>
       <boxGeometry attach="geometry" args={[3, 3, 3]} />
-      <meshStandardMaterial map={colorMap} />
+      {/* <meshStandardMaterial map={colorMap} /> */}
+      <meshNormalMaterial attach="material" />
     </mesh>
   );
 };
